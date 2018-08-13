f446bec53bcadde884e1fbfcd19aa5dbeb432508
Salvatore Novelli <salvatore.novelli@gmail.com>
2018-08-13 17:33:46 +0100

    init

---
+++ src/main/java/com/myseotoolbox/googleanalyticsservice/GoogleAnalyticsServiceApplication.java
@@ -0,0 +1,11 @@
+package com.myseotoolbox.googleanalyticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAnalyticsServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleAnalyticsServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/myseotoolbox/googleanalyticsservice/GoogleAnalyticsServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myseotoolbox.googleanalyticsservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoogleAnalyticsServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
